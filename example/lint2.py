916f520adb75198853d44d31ba9b1ccf5f4cf85e
MathisLec <45670636+MathisLec@users.noreply.github.com>
2023-06-29 17:44:22 +0200

    Create lint2.py

---
+++ example/lint2.py
@@ -0,0 +1,13 @@
+# This file is used to test the linting functionality of the project
+unused_variable = 1
+
+
+def printHello():
+    print('Hello')
+
+def printWorld():
+    print('World')
+
+def print_hello_world():
+    printHello()
+    printWorld() 
